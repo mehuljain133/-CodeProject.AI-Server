f6dd5d5de2aec8333affc64faef9de4b1e4dd63e
mehul jain <75320785+mehuljain133@users.noreply.github.com>
2026-02-05 15:32:26 +0530

    Add files via upload

---
+++ modules/ObjectDetectionYOLOv5Net/YOLOv5/YoloLabel.cs
@@ -0,0 +1,11 @@
+﻿namespace Yolov5Net.Scorer
+{
+    /// <summary>
+    /// Label of detected object.
+    /// </summary>
+    public struct YoloLabel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
